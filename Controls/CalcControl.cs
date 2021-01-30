981714537b02ea877781a1c940128d80f7358b65
n_zhenek@mail.ru <n_zhenek@mail.ru>
2021-01-30 17:28:09 +0400

    Добавьте файлы проекта.

---
+++ Controls/CalcControl.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace WPFCalc.Controls
+{
+
+    public class CalcControl
+    {
+        public BindingList<string> TableList = new BindingList<string>();
+        public BindingList<string> CalcList = new BindingList<string>();
+
+        public string ShowList()
+        {
+            string Numbers = default;
+            foreach (string item in TableList)
+            {
+                Numbers = Numbers + item;
+            }
+
+            return Numbers;
+        }
+
+
+        public void AddToList(string value)
+        {
+            TableList.Add(value);
+        }
+
+
+        public void CalcAction(string value)
+        {
+            string action = value;
+            string numb = ShowList();
+            CalcList.Add(numb);
+            CalcList.Add(action);
+            TableList.Clear();
+        }
+
+        public void GetRadical()
+        {
+            if (TableList.Count > 0)
+            {
+                double value = Convert.ToDouble(ShowList());
+                string number = Math.Sqrt(value).ToString();
+                TableList.Clear();
+                TableList.Add(number);
+            }
+        }
+
+        public string OneDivideByX()
+        {
+            if (TableList.Count > 0)
+            {
+                double value = Convert.ToDouble(ShowList());
+                if (value != 0)
+                {
+                    string number = (1 / value).ToString();
+                    TableList.Clear();
+                    TableList.Add(number);
+                    return number;
+                }
+                else
+                {
+                    TableList.Clear();
+                    return "Деление на 0";
+                }
+            }
+            else { return "Невозможная операция"; }
+        }
+
+
+        public string ChangeSign()
+        {
+            double value = Convert.ToDouble(ShowList());
+            string number = Convert.ToString(value + (value * -2));
+            TableList.Clear();
+            TableList.Add(number);
+            return Convert.ToString(value + (value * -2));
+        }
+
+        public void DelChar()
+        {
+            TableList.Remove(TableList.LastOrDefault());
+        }
+
+        public string Calculate()
+        {
+            string numb = ShowList();
+            CalcList.Add(numb);
+            TableList.Clear();
+            double result = default;
+            while (CalcList.Count > 0)
+            {
+                if (CalcList[1] == "+")
+                {
+                    result = Convert.ToDouble(CalcList[0]) + Convert.ToDouble(CalcList[2]);
+                    CalcList.RemoveAt(2);
+                    CalcList.RemoveAt(1);
+                    CalcList.RemoveAt(0);
+                    if (CalcList.Count > 1)
+                    { CalcList.Insert(0, Convert.ToString(result)); }
+                }
+                else if (CalcList[1] == "-")
+                {
+                    result = Convert.ToDouble(CalcList[0]) - Convert.ToDouble(CalcList[2]);
+                    CalcList.RemoveAt(2);
+                    CalcList.RemoveAt(1);
+                    CalcList.RemoveAt(0);
+                    if (CalcList.Count > 1)
+                    { CalcList.Insert(0, Convert.ToString(result)); }
+                }
+
+                else if (CalcList[1] == "*")
+                {
+                    result = Convert.ToDouble(CalcList[0]) * Convert.ToDouble(CalcList[2]);
+                    CalcList.RemoveAt(2);
+                    CalcList.RemoveAt(1);
+                    CalcList.RemoveAt(0);
+                    if (CalcList.Count > 1)
+                    { CalcList.Insert(0, Convert.ToString(result)); }
+                }
+                else if (CalcList[1] == "/")
+                {
+                    if (Convert.ToDouble(CalcList[2]) != 0)
+                    {
+                        result = Convert.ToDouble(CalcList[0]) / Convert.ToDouble(CalcList[2]);
+                        CalcList.RemoveAt(2);
+                        CalcList.RemoveAt(1);
+                        CalcList.RemoveAt(0);
+                        if (CalcList.Count > 1)
+                        { CalcList.Insert(0, Convert.ToString(result)); }
+                    }
+                    else
+                    {
+                        CalcList.Clear();
+                        TableList.Clear();
+                        return "Деление на 0";
+                    }
+                }
+            }
+            TableList.Add(Convert.ToString(result));
+
+            return Convert.ToString(result);
+        }
+
+
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFCalc.Controls;
+
+namespace WPFCalc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CalcControl control = new CalcControl();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+            control.TableList.ListChanged += TableList_ListChanged;
+
+        }
+
+        private void TableList_ListChanged(object sender, System.ComponentModel.ListChangedEventArgs e)
+        {
+            Table.Text = control.ShowList();
+        }
+
+        private void One_Click(object sender, RoutedEventArgs e)
+        {
+            control.AddToList(One.Content.ToString());
+            e.Handled = true;
+        }
+
+        private void Two_Click(object sender, RoutedEventArgs e)
+        {
+            control.AddToList(Two.Content.ToString());
+            e.Handled = true;
+        }
+
+        private void Three_Click(object sender, RoutedEventArgs e)
+        {
+            control.AddToList(Three.Content.ToString());
+            e.Handled = true;
+        }
+
+        private void Four_Click(object sender, RoutedEventArgs e)
+        {
+            control.AddToList(Four.Content.ToString());
+            e.Handled = true;
+        }
+
+        private void Five_Click(object sender, RoutedEventArgs e)
+        {
+            control.AddToList(Five.Content.ToString());
+            e.Handled = true;
+        }
+
+        private void Six_Click(object sender, RoutedEventArgs e)
+        {
+            control.AddToList(Six.Content.ToString());
+            e.Handled = true;
+        }
+
+        private void Seven_Click(object sender, RoutedEventArgs e)
+        {
+            control.AddToList(Seven.Content.ToString());
+            e.Handled = true;
+        }
+
+        private void Eight_Click(object sender, RoutedEventArgs e)
+        {
+            control.AddToList(Eight.Content.ToString());
+            e.Handled = true;
+        }
+
+        private void Nine_Click(object sender, RoutedEventArgs e)
+        {
+            control.AddToList(Nine.Content.ToString());
+            e.Handled = true;
+        }
+
+        private void Zero_Click(object sender, RoutedEventArgs e)
+        {
+            control.AddToList(Zero.Content.ToString());
+            e.Handled = true;
+        }
+
+        private void dot_Click(object sender, RoutedEventArgs e)
+        {   if (control.TableList.Count == 0)
+            {
+                Table.Text = "Error";
+            }
+            else
+            {
+                control.AddToList(dot.Content.ToString());
+                e.Handled = true;
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            control.TableList.Clear();
+            control.CalcList.Clear();
+            e.Handled = true;
+        }
+
+        private void Plus_Click(object sender, RoutedEventArgs e)
+        {
+            control.CalcAction(Plus.Content.ToString());
+            e.Handled = true;
+        }
+
+        private void Minus_Click(object sender, RoutedEventArgs e)
+        {
+            control.CalcAction(Minus.Content.ToString());
+            e.Handled = true;
+        }
+
+        private void Multiply_Click(object sender, RoutedEventArgs e)
+        {
+            control.CalcAction(Multiply.Content.ToString());
+            e.Handled = true;
+        }
+
+        private void Divide_Click(object sender, RoutedEventArgs e)
+        {
+            control.CalcAction(Divide.Content.ToString());
+            e.Handled = true;
+        }
+
+        private void Count_Click(object sender, RoutedEventArgs e)
+        {
+            Table.Text = control.Calculate();
+            e.Handled = true;
+        }
+
+        private void PlusMinus_Click(object sender, RoutedEventArgs e)
+        {
+            Table.Text = control.ChangeSign();
+            e.Handled = true;
+        }
+
+        private void DelChar_Click(object sender, RoutedEventArgs e)
+        {
+            control.DelChar();
+            e.Handled = true;
+        }
+
+        private void Radical_Click(object sender, RoutedEventArgs e)
+        {
+            control.GetRadical();
+            e.Handled = true;
+        }
+
+        private void OneDivideByX_Click(object sender, RoutedEventArgs e)
+        {
+            Table.Text = control.OneDivideByX();
+            e.Handled = true;
+        }
+    }
+}
